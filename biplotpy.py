107397de5cdb583f3046845db6014b3d387683b3
Victor Vicente Palacios <victorvicpal@gmail.com>
2017-10-31 22:41:43 +0100

    changes 2

--- biplotpy.py
+++ biplotpy.py
@@ -10,11 +10,18 @@ class biplotpy:
 	Gabriel Biplots
 	'''
 
-	def __init__(self, data):
+	def __init__(self, data,dim,alpha = 1):
 		self.data = data
+		self.dim = dim
+		self.alpha = alpha
+		self.p = self.data.shape[1]
+		self.n = self.data.shape[0]
 
 	def standardize(self):
 		medias = self.data.mean(axis=0)
 		desv = self.data.std(axis=0)
 		self.data_st = (self.data-medias)/desv
 		return self.data_st
+
+	def SVD(self.data):
+
