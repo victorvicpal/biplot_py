7064902e86a28cfe10ce278eb331402771c15e91
Victor Vicente Palacios <victorvicpal@gmail.com>
2017-10-31 23:07:11 +0100

    changes 4

--- biplotpy.py
+++ biplotpy.py
@@ -12,16 +12,18 @@ class biplotpy:
 
 	def __init__(self, data,dim,alpha = 1):
 		self.data = data
+		self.p = self.data.shape[1] #elements
+		self.n = self.data.shape[0] #variables
 		if isinstance(dim, (int, float)):
 			self.dim = dim
 		else:
 			raise ValueError('not numeric')
+		if self.dim>self.p:
+			raise ValueError('dim bigger than p')
 		if (alpha>=0 and alpha<=1):
 			self.alpha = alpha
 		else:
 			raise ValueError('not between 0 and 1')
-		self.p = self.data.shape[1] #elements
-		self.n = self.data.shape[0] #variables
 
 	def standardize(self):
 		medias = self.data.mean(axis=0)
