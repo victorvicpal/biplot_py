d054b3f809d1d1e25137aeaf859547e5b04f0ad8
Victor Vicente Palacios <victorvicpal@gmail.com>
2017-10-31 22:24:33 +0100

    Python class

---
+++ biplotpy.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+import pandas as pd
+import numpy as np
+from sklearn.utils.extmath import randomized_svd
+import matplotlib.pyplot as plt
+
+class biplotpy:
+	'''
+	Gabriel Biplots
+	'''
+
+	def __init__(self, data):
+		self.data = data
+
+	def standardize(self):
+		medias = self.data.mean(axis=0)
+		desv = self.data.std(axis=0)
+		self.data_st = (self.data-medias)/desv
