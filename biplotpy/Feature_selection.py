d83c879928e5199be4b42ce8c96b2b2482f95f92
victorvicpal <victorvicpal@gmail.com>
2018-02-02 14:04:01 +0100

    outliers detection module added to Feature_selection

--- biplotpy/Feature_selection.py
+++ biplotpy/Feature_selection.py
@@ -3,13 +3,14 @@ import CanonicalBip
 import pandas
 import numpy
 import itertools
+import warnings
 
 class Feature_selection(object):
 	'''
 	Feature selection for classification problems
 	'''
 
-	def __init__(self, bip, target, thr_dis = 75, thr_corr = 0.89, type_cor = "global"):
+	def __init__(self, bip, target, thr_dis = 75, thr_corr = 0.89, type_cor = "global", drop_outliers = False, whisk = 1.8):
 		if isinstance(bip, ClassicBip.ClassicBip):
 			__type__ = "Classic"
 			self.bip = bip
@@ -41,6 +42,7 @@ class Feature_selection(object):
 			Project = bip.Ind_Coord.dot(bip.Var_Coord.T)
 			C = bip.Var_Coord
 
+
 		# Positive rescalation of projections
 
 		v_min = numpy.array([abs(el) if el < 0 else el for el in Project.min(axis=0)])
@@ -49,6 +51,41 @@ class Feature_selection(object):
 			Project[:,i] = proj + v_min[i]
 		
 		classes = numpy.unique(target)
+
+		def get_outliers(d, whis):
+			q1 = numpy.percentile(d, 25)
+			q3 = numpy.percentile(d, 75)
+			iq = q3 - q1
+			hi_val = q3 + whis * iq
+			wisk_hi = numpy.compress(d <= hi_val, d)
+			if len(wisk_hi) == 0 or numpy.max(wisk_hi) < q3:
+				wisk_hi = q3
+			else:
+				wisk_hi = max(wisk_hi)
+			# get low extreme
+			lo_val = q1 - whis * iq
+			wisk_lo = numpy.compress(d >= lo_val, d)
+			if len(wisk_lo) == 0 or numpy.min(wisk_lo) > q1:
+				wisk_lo = q1
+			else:
+				wisk_lo = min(wisk_lo)
+			return list(numpy.where(d > wisk_hi)[0])+list(numpy.where(d < wisk_lo)[0])
+
+		if drop_outliers == True:
+			outliers = []
+			for var in Project.T:
+				outliers = outliers + get_outliers(var,whisk)
+
+			outliers = list(set(outliers))
+			self.outliers_ind = outliers
+
+			perc_drop = len(outliers)*100/bip.data.shape[0]
+			Project = numpy.delete(Project, (outliers), axis=0)
+			target = numpy.delete(target, (outliers), axis=0)
+
+			if  perc_drop > 5.5:
+				warnings.warn("You're dropping %s\%  of the data. Try to increase 'whisk'" % perc_drop)
+
 
 		# Tracking class index
 
@@ -73,7 +110,6 @@ class Feature_selection(object):
 		for i, cmb in enumerate(comb):
 			Disc[:,i] = abs(Project[IND[cmb[0]]].mean(axis=0) - Project[IND[cmb[1]]].mean(axis=0))
 
-		self.Disc = Disc
 
 		# Drop correlated variables
 
@@ -84,7 +120,6 @@ class Feature_selection(object):
 					POS.append(i)
 		POS = list(set(POS))
 
-		self.POS_not = POS
 
 
 		if type_cor == "global":
